8f60e694dddcee378573d7fd2e4d07626283ca72
Marcin Lawski <lawski.marcin@gmail.com>
2026-07-27 08:23:46 +0200

    fix: add missing tree-sitter-cli for installation
    tree-sitter-cli is required to build fastbuild custom parser

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -41,7 +41,7 @@ map("n", "<leader>mp", ":MarkdownPreview<CR>", { desc = "Markdown Preview" })
 map("n", "<leader>ms", ":MarkdownPreviewStop<CR>", { desc = "Markdown Preview Stop" })
 
 -- ============================
---  C++ TOOLS & FUNCY
+--  C++ TOOLS
 -- ============================
 
 -- nvim-treesitter-cpp-tools
